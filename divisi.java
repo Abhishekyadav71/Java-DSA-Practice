9dd87820b471618970a68805df705e12d15965a6
Abhishek Yadav <lagendyadav12@gmail.com>
2026-06-28 19:39:49 +0530

    Add files via upload

---
+++ divisi.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class divisi {
+    public static void main (String[] args){
+        Scanner abhishek = new Scanner(System.in);
+        System.out.print("enter a number");
+        int n = abhishek .nextInt();
+        if(n%5==0)System.out.println("divisible by 5");
+        else System.err.println("not divisible by 5");
+        abhi.close();
+    }
+}
