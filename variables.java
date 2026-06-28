ea2a9409831b20c7026ab8a926e733b9e71b3825
Abhishek Yadav <lagendyadav12@gmail.com>
2026-06-28 19:20:59 +0530

    Add files via upload

---
+++ variables.java
@@ -0,0 +1,11 @@
+public class variables {
+    public static void main(String [] args ){
+        double l = 10;
+        double b = 5;
+        double h = 4;
+        double Tsa = 2*(l*b+b*h+l*b);
+
+System.out.println("total surface area of cubiod = " + Tsa);
+        }
+    
+}
